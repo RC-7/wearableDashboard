fe11729f7893fcd42bd77e81a7cac92b98fca7a4
RC-7 <rashcassim@gmail.com>
2021-04-23 14:57:50 +0200

    Added more data to README and code cleanup

--- README.md
+++ README.md
@@ -1,9 +1,62 @@
 # Wearable metric dashboard
 
+Website can be access [here](https://main.d2ewd5v9ij08do.amplifyapp.com/)
+
+Note that there is no data for the last month, data starts from: `2021-03-21`
+
+### Overview diagram:
+![img](overview.png)
+
+### Stack used:
+- Lambda's:
+    - Python 3.7 with pandas
+- UI
+    - React and Node
+    - CSS
+    - Jest was setup for testing but not implimented
+- AWS 
+    - Lambda
+    - S3
+    - Dynamodb
+    - Amplify
+    - Api Gateway
+    - IAM
+    - Cloudwatch Logging
+
 ### File structure:
 - `lambda` directory contains the lambda code (python) used to ingest and querry data
-  - getWearableData is the lambda function connected to an API. This API is used to querry data stored in Dynamo DB.
-  - ingestionLambda is the lambda used to batch ingest data from a file placed in an S3 bucket.
-- The rest of the repo is oriented towards the website.
+  - getWearableData is the lambda function connected to an API. This API is used to querry data stored in Dynamo DB. The API uses AWS Api Gateway to allow the lambda to be hit. We can provide the API with a date range to querry and the columnValues/metrics that we want to view. 
+  - An example curl and response used to hit the API is:
+    - `curl -X POST -H "x-api-key:P20NOZjnTP20YCT1WsKAb9pG8o3oVHj25kHK62N1" -H "Content-Type: application/json" https://27k379fu03.execute-api.eu-west-1.amazonaws.com/prod/getWearableData -d "{\"date\": \"2021-03-13,2021-03-19\",\"columnValues\": \"AL, CL\"}"`
+    - `{"AL": [39.8242486030958, 40.951995846465, 46.3475261987231, 36.3846603062292, 38.1569252846543], "CL": [36.2065369428448, 36.8012046612011, 37.461086631596, 35.6837346645221, 36.4048584265747]}`
+  - ingestionLambda is the lambda used to batch ingest data to Dynamo DB from a file/object placed in an S3 bucket.
+- The rest of the repo is oriented towards the website. View App.jsx for the react code driving the website.
+- data/testData was used to test the ingestionLambda and is a subset of the overall dataset
+
 ### Running website:
 `Run npm start`
+
+
+### Short term improvemets I should have liked to make:
+- UI
+    - Use chart.js as the graph library instead, I discovered too late that the labels options mentioned on the git page of the library I used were not present and I could not angle the x axis labels. Therefore the x axis labels of the graph are the "Activity number" and note the start date and time of the activity due to labels overlapping. I have console log'd the labels to show what was intended for the graphs.
+    - Display data as a table as well/give the option to switch betwen a table and graoh view.
+    - Create a grid to display graphs in closer proximity to each other or not based on how much data is provided.
+    - Use a different graph library to allow scrolling so data doesnt become squashed when viewed for a year.
+    - Use a Date range picker instead of string.
+    - Display the number of workouts in different time buckets on a overall global graph/summary table, possibly by month.
+    - Display each activity type as a different colour on the graph when `all` is selected.
+    - Add testing.
+    - General UI usability improvements.
+    - Don't display labels when lots of data is returned.
+    - Validate inputs.
+- Backend
+    - API to ingest just one datapoint.
+    - Add User ID to DB so multiple users can be on the same system/table.
+    - Some intellegence on the API to return aggregate data/analysed data.
+    - Scaling Lambda and DB and evaluating optomisations.
+    - Rework the table to optomise more searches, change GSI.
+    - Add more security on the API.
+    - Add more response code's to make API errors more user readable and usable.
+    - Backup tables for redundancy.
+    - Create API Docs.
--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,6 @@ const activities =[
   {value: 'yoga', label: 'yoga'},
   {value: 'other', label: 'other'},
 ]
-
 const columns = [
   {value: 'maxHRAge', label: 'maxHRAge'},
   {value: 'duration_mins', label: 'duration_mins'},
@@ -42,19 +41,13 @@ const columns = [
   {value: 'AL', label: 'AL'},
   {value: 'CL', label: 'CL'},
 ]
-
 const deaultColumns = [{value: 'vo2Max', label: 'vo2Max'},
   {value: 'AL', label: 'AL'},
   {value: 'CL', label: 'CL'}
 ];
-
 const defaultActivity = [{value: 'all', label: 'all'},]
 
-const HEADERS = {headers: {
-  "x-api-key":"x-api-key",
-  "Content-Type": "application/json",
-  "Access-Control-Allow-Origin": "*",
-}}
+const dateRegex = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/g;
 const ENDPOINT = "https://api/endpoint"
 
 class App extends React.Component {
@@ -63,13 +56,14 @@ class App extends React.Component {
       super(props)
       let todayDate = new Date()
       this.state = {
-        data: [10,0,-2.5,540],
         selectedActivity: 'all',
-        selectedColumns: 'AL, CL, vo2Max',
+        selectedColumns: 'AL, CL, vo2Max, startTimeLocal',
         dateLower : `${todayDate.getFullYear()}-${('0' + todayDate.getMonth()).slice()}-${todayDate.getDate()}`,
         dateUpper: new Date().toISOString().slice(0, 10),
-        dataFromAPI: {},
-        noDataText: ""
+        metricDataFromAPI: {},
+        datesOfMetricData: [],
+        noDataText: "",
+        validationError: null
       };
     }
 
@@ -81,15 +75,18 @@ class App extends React.Component {
       if (activityType !== 'all') querry['activityType'] = activityType
       axios.post(ENDPOINT, querry, {
         headers: {
-          "x-api-key":"P20NOZjnTP20YCT1WsKAb9pG8o3oVHj25kHK62N1",
+          "x-api-key":"x-api-key",
           "Content-Type": "application/json",
         }
       }
     ).then((res) => {
+      let dates = res.data['startTimeLocal']
+      console.log('date\'s of metrics', dates)
+      delete res.data['startTimeLocal']
       if (res.data[Object.keys(res.data)[0]].length > 0){
-        this.setState({dataFromAPI: res.data, noDataText: ""})
+        this.setState({metricDataFromAPI: res.data, noDataText: "", datesOfMetricData: dates})
       } else {
-        this.setState({dataFromAPI: {}, noDataText: "No data for date range selected"})
+        this.setState({metricDataFromAPI: {}, noDataText: "No data for date range selected"})
       }
       
       })
@@ -101,9 +98,22 @@ class App extends React.Component {
     handleRequestData = () => {
 
       let {selectedActivity, dateLower, dateUpper, selectedColumns } = this.state
-      
+      if (!dateLower.match(dateRegex) && !dateUpper.match(dateRegex)){
+        this.setState({validationError: 'Upper and lower date values are not valid. This can only be valid days in: yy\
+        yy-mm-dd or yyyy-mm-dd hh:mm:ss format'})
+        return
+      } else if (!dateLower.match(dateRegex)){
+        this.setState({validationError: 'Lower date value is not valid. This can only be valid days in: yy\
+        yy-mm-dd or yyyy-mm-dd hh:mm:ss format'})
+        return
+      } else if (!dateUpper.match(dateRegex)){
+        this.setState({validationError: 'Upper date value is not valid. This can only be valid days in: yy\
+        yy-mm-dd or yyyy-mm-dd hh:mm:ss format'})
+        return
+      }
+  
       this.querryAPI(selectedActivity, `${dateLower}, ${dateUpper}`, selectedColumns)
-
+      this.setState({validationError: null})
     };
     handleActivitySelect = (e) => {
       let activities = []
@@ -125,45 +135,47 @@ class App extends React.Component {
     handleChangeColumn = (e) => {
       let columns = ""
       for (var column in e){
-        columns += (e[column].value)
+        columns += (e[column].value) + ' , '
       }
+      // columns =columns.slice(0,-2)  
+      columns += 'startTimeLocal'
       this.setState({selectedColumns: columns})
     }
 
-    generateGraph(data, title){
+    generateGraph(data, title, datesOfMetricData){
       let height = window.screen.height *0.5
       let width = window.screen.width*0.7
       return (
         <div>
-        <h2 class = 'graph'>{title}</h2>
-        <VerticalBarGraph 
-        width={width}
-        height={height}
-        data={data}
-        hasXAxisLabels={false}
-        style={{
-        marginBottom: 30,
-        padding: 10,
-        paddingTop: 20,
-        borderRadius: 20,
-        backgroundColor: `#dbf0ef`
-      }}
-      
-      />
+          <h2 className = 'graph'>{title}</h2>
+
+          <VerticalBarGraph 
+          width={width}
+          key = {`graph#${title}`}
+          height={height}
+          data={data}
+          hasXAxisLabels={false}
+          style={{
+          marginBottom: 30,
+          padding: 10,
+          paddingTop: 20,
+          borderRadius: 20,
+          backgroundColor: `#dbf0ef`
+        }}
+        />
       </div>
       )
     }
   
     render() {
-      console.log(this.state.selectedActivity)
-      let {dataFromAPI, noDataText} = this.state;
+      let {metricDataFromAPI, noDataText, validationError, datesOfMetricData} = this.state;
       let graphs = []
-      for (var key in dataFromAPI){
-        graphs.push(this.generateGraph(dataFromAPI[key], key))
+      for (var key in metricDataFromAPI){
+        graphs.push(this.generateGraph(metricDataFromAPI[key], key, datesOfMetricData))
       }
       return (
         <>
-        <div class="card">
+        <div className="card">
            <h1>Wearable metrics dashboard</h1>
            <h2>Activity</h2>
            <Select 
@@ -182,14 +194,14 @@ class App extends React.Component {
               defaultValue={deaultColumns}
               onChange={this.handleChangeColumn}/>
            <h2>Date rage</h2>
-           <input type="text" class = "dateSelector" value={this.state.dateLower} padding = '10px' onChange={this.handleChangeLowerDate} />
+           <input type="text" className = "dateSelector" value={this.state.dateLower} padding = '10px' onChange={this.handleChangeLowerDate} />
            <span>to</span>
-           <input type="text" class = "dateSelector" value={this.state.dateUpper} onChange={this.handleChangeUpperDate} />
+           <input type="text" className = "dateSelector" value={this.state.dateUpper} onChange={this.handleChangeUpperDate} />
            <button onClick={this.handleRequestData}>Get results</button>
            <div>
-              <span>
-              {noDataText}
-              </span>
+              <p>
+              {validationError? validationError: noDataText}
+              </p>
            </div>
            {graphs}
         </div>
